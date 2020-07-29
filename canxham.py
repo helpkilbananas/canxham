beeaf811bebf4a76b36b337760be1fc722df2e15
Jason Roddan <helpkilbananas@gmail.com>
2020-07-29 10:18:33 -0230

    Minor changes/improvements

--- canxham.py
+++ canxham.py
@@ -7,13 +7,7 @@ import os
 
 class Exam:
     def __init__(self):
-        self.pwd = os.path.dirname(__file__)
-        self.score = 0
-        self.questions = []
-        if len(argv) == 2 and argv[1] in ["adv", "basic"]:
-            self.names = f"amat_{argv[1]}_quest"
-            self.url = f"http://apc-cap.ic.gc.ca/datafiles/{self.names}.zip"
-        else:
+        if len(argv) != 2 and argv[1] not in ["adv", "basic"]:
             print("\nUsage:  canxham.py  'basic' { or } 'adv'  \n")
             exit(1)
 
@@ -21,30 +15,32 @@ class Exam:
             self.question_total = 50
         else:
             self.question_total = 100
+        
+        self.pwd = os.path.dirname(__file__)
+        self.file_name = f"amat_{argv[1]}_quest"
+        self.url = f"http://apc-cap.ic.gc.ca/datafiles/{self.file_name}.zip"
+
+        self.score = 0
 
     def update(self):
         print(f"Fetch latest {argv[1]} exam data {self.url}...")
         download = r.get(self.url)
         print("Done\n")
         
-        with open(f"{self.pwd}{self.names}.zip", "wb") as f:
+        with open(f"{self.pwd}{self.file_name}.zip", "wb") as f:
             f.write(download.content)
-        zip_file = zipfile.ZipFile(f"{self.pwd}{self.names}.zip")
-        zip_file.extract(f"{self.names}_delim.txt")
+        zip_file = zipfile.ZipFile(f"{self.pwd}{self.file_name}.zip")
+        zip_file.extract(f"{self.file_name}_delim.txt")
+        os.remove(f"{self.pwd}{self.file_name}.zip")
 
-        with open(f"{self.pwd}{self.names}_delim.txt", "rb") as f:
+        with open(f"{self.pwd}{self.file_name}_delim.txt", "rb") as f:
             data = f.readlines()
-        d = [i.decode("latin").strip("\r\n").split(";") + ["\r"] for i in data]
-        self.data = sorted(d)
-
-        with open(f"{self.pwd}{self.names}.txt", "w") as f:
-            for i in self.data:
-                f.write(f'{";".join(i)}\r\n')
+        d = [i.decode("latin").split(";") + ["\r"] for i in data]
+        self.data = [f'{";".join(i)}' for i in sorted(d)]
+        os.remove(f"{self.pwd}{self.file_name}_delim.txt")
 
     def start(self):
         self.date = dt.utcnow().strftime("%Y-%m-%d-%H%M")
-        with open(f"{self.pwd}{self.names}.txt", "r") as f:
-            self.data = f.readlines()
 
         data = []
         for line in self.data:
@@ -53,31 +49,31 @@ class Exam:
                 data.append(sections[i].replace("\r", ""))
         self.data = data
 
-        self.quests = []
+        question_list = []
         for i in range(self.question_total):
-            quest = rand(self.data)
-            while f"{argv[1][0].upper()}-00" not in quest:
-                quest = rand(self.data)
+            question = rand(self.data)
+            while f"{argv[1][0].upper()}-00" not in question:
+                question = rand(self.data)
             
-            self.data.pop(self.data.index(quest))
+            self.data.pop(self.data.index(question))
             
-            quest = ";".join(quest.split(";")[:6])
-            self.quests.append(quest.split(";"))
+            question = ";".join(question.split(";")[:6])
+            question_list.append(question.split(";"))
             
-            question = "\n".join(self.quests[i][:2]).upper()
-            answers = self.quests[i][2:]
+            question = "\n".join(question_list[i][:2]).upper()
+            answers = question_list[i][2:]
             correct = answers[0]
             
-            ans = []
+            answer_list = []
             for j in range(4):
-                ans.append(rand(answers))
-                answers.pop(answers.index(ans[j]))
+                answer_list.append(rand(answers))
+                answers.pop(answers.index(answer_list[j]))
             
             print(f"{question}\n")
             
             abc = ["a", "b", "c", "d"]
-            for j in range(len(ans)):
-                print(f"{abc[j]}):  {ans[j]}")
+            for j in range(len(answer_list)):
+                print(f" {abc[j]}):  {answer_list[j]}\n")
             
             try:
                 answered = str(input("\nYour Answer: ").lower())
@@ -92,24 +88,22 @@ class Exam:
                 print(f"\nScore:  {score}%\n")
                 exit(2)
 
-            if ans[abc.index(answered)] == correct:
+            if answer_list[abc.index(answered)] == correct:
                 print(f"Correct!  The answer is:\n{correct} ({answered})\n")
                 self.score += 1
             else:
-                print(f"Incorrect!  The answer is:\n{correct} ({abc[ans.index(correct)]})\n")
+                print(f"Incorrect!  The answer is:\n{correct} ({abc[answer_list.index(correct)]})\n")
             
-            questions = [f"\n#{i + 1} {question}\n", 
-                        [f"{abc[j]}):  {ans[j]}\n" for j in range(len(ans))],
-                        f"\nAnswered:  {ans[abc.index(answered)]} ({answered})\n",
-                        f"\nCorrect:  {correct} ({abc[ans.index(correct)]})\n",
-                        f"\nCurrent score: {self.score}",
-                        f"\nCurrent mark: {self.score / self.question_total * 100}%\n"]
+            exam_log = [f"\n#{i + 1} {question}\n", 
+                       [f"{abc[j]}):  {answer_list[j]}\n" for j in range(len(answer_list))],
+                       f"\nAnswered:  {answer_list[abc.index(answered)]} ({answered})\n",
+                       f"\nCorrect:  {correct} ({abc[answer_list.index(correct)]})\n",
+                       f"\nCurrent score: {self.score}",
+                       f"\nCurrent mark: {self.score / self.question_total * 100}%\n"]
 
-            self.questions.append(questions)
-
-            with open(f"{self.pwd}{argv[1]}_exam-{self.date}.txt", "a") as f:
-                for j in range(len(questions)):
-                    f.write("".join(questions[j]))
+            with open(f"{self.pwd}{argv[1]}_EXAM-{self.date}.txt", "a") as f:
+                for j in range(len(exam_log)):
+                    f.write("".join(exam_log[j]))
                     
 
 exam = Exam()
